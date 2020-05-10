2bd0ad1102558ce09aa17c04ade7e4cdf0c632f1
Stricks1 <gmalheiross@gmail.com>
2020-05-10 17:39:19 -0300

    change length to size on methods and created my_all? method

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ module Enumerable
     return to_enum unless block_given?
 
     i = 0
-    while i < length
+    while i < size
       yield self[i]
       i += 1
     end
@@ -14,7 +14,7 @@ module Enumerable
     return to_enum unless block_given?
 
     i = 0
-    while i < length
+    while i < size
       yield self[i], i
       i += 1
     end
@@ -26,18 +26,36 @@ module Enumerable
 
     new_array = []
     i = 0
-    while i < length
+    while i < size
       new_array << self[i] if yield self[i]
       i += 1
     end
     new_array
+  end
+
+  def my_all?
+    i = 0
+    unless block_given?
+      someNull = false
+      while i < size
+        someNull = true if self[i] == false || self[i] == nil
+        i += 1
+      end
+      return !someNull
+    end
+    someNull = false
+    while i < size
+      someNull = true unless yield self[i]
+      i += 1
+    end
+    return !someNull
   end
 end
 
 # array = %w[4 3 78 2 0 2]
 
 # p(array.each { |x| p "Element #{x}" })
-# p(array.my_each { |x| p "Element #{x}" })
+#p(array.my_each { |x| p "Element #{x}" })
 
 # p(array.each_with_index { |x, y| p "We have #{x} with index #{y}" })
 # p(array.my_each_with_index { |x, y| p "We have #{x} with index #{y}" })
@@ -46,6 +64,9 @@ end
 # puts "select method : #{a.select { |v| v =~ /[aeiou]/ }}\n\n" #=> ["a", "e"]
 # puts "select method my : #{a.my_select { |v| v =~ /[aeiou]/ }}\n\n" #=> ["a", "e"]
 
-# c = [18, 22, 3, 3, 53, 6]
+c = [18, 22, 3, 3, 53, 6]
 # puts "select method : #{c.select(&:even?)}\n\n"
 # puts "select method my : #{c.my_select(&:even?)}\n\n"
+
+puts "all? method : #{c.all? { |x| x > 3 }}\n\n"
+puts "all method my : #{c.my_all? { |x| x > 3 }}\n\n"
