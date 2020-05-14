8b3af55aee06f1b1ca4ddb1ad83965aa59641c20
rindra josia <rrindrajosia@gmail.com>
2020-05-14 23:40:47 +0300

    test my_all and my_select

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -3,6 +3,8 @@ require "./enumerables.rb"
 describe Enumerable do
   let(:test_array1) { [11, 2, 3, 56] }
   let(:test_array2) { %w[a b c d] }
+  let(:a) { %w[a b c d e f] }
+  let(:c) { [18, 22, 3, 3, 53, 6]}
   let(:true_array) { [1, true, "hi", []] }
   let(:false_array) { [nil, false, nil, false] }
   let(:words) { %w[dog door rod blade] }
@@ -40,4 +42,22 @@ describe Enumerable do
       expect(test_array1.my_each_with_index { |x, y| p "item: #{x}, index: #{y}" }).to eql(test_array1.each_with_index { |x, y| p "item: #{x}, index: #{y}" })
     end
   end
+
+  describe "#my_select" do
+    it "To select element do a block code" do
+      expect(a.my_select { |v| v =~ /[aeiou]/ }).to eql(a.select { |v| v =~ /[aeiou]/ })
+    end
+    it "To select even numbers" do
+      expect(c.my_select(&:even?)).to eql(c.select(&:even?))
+    end
+  end
+
+  describe "#my_all?" do
+    it "To check if all numbers are >= 3" do
+      expect(c.my_all? { |x| x > 3 }).to eql(c.all? { |x| x > 3 })
+    end
+  end
+
+
+
 end
